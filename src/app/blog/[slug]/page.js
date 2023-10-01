20c2045627378bbd03a42d73c5f6d2aa140b1cc6
kazuki <kazuki.sasahara.07@gmail.com>
2023-10-01 20:22:00 +0900

    詳細ページをスタイリング

--- src/app/blog/[slug]/page.js
+++ src/app/blog/[slug]/page.js
@@ -1,35 +1,41 @@
-import Image from 'next/image'
-import Link from 'next/link'
-import contentfulClient from '@/lib/contentful'
-import RichText from '@/lib/RichText'
+import Image from "next/image";
+import Link from "next/link";
+import contentfulClient from "@/lib/contentful";
+import BlogRichText from "@/lib/RichText";
 
 export async function generateStaticParams() {
   const entries = await contentfulClient.getEntries({
-		content_type: 'blog'
-	})
-  const blogs = entries.items
-  return blogs.map((blog) => ({ slug: blog.fields.slug }))
+    content_type: "blog",
+  });
+  const blogs = entries.items;
+  return blogs.map((blog) => ({ slug: blog.fields.slug }));
 }
-  
-
 
 export default async function BlogPage({ params }) {
+  const entries = await contentfulClient.getEntries({
+    content_type: "blog",
+    "fields.slug": params.slug,
+  });
 
-const entries = await contentfulClient.getEntries({
-		content_type: 'blog',
-    'fields.slug': params.slug,
-	})
+  const { title, slug, body } = entries.items[0].fields;
 
-  const blog = entries.items[0]
-
-  return(
-<main className="flex min-h-screen flex-col items-center justify-between p-24">
-    <div>
-      <p>{blog.fields.title}</p>
-      <RichText document={blog.fields.body} />
-    </div>
-  </main>
-  )
-
-  
+  return (
+    <main className="container mx-auto p-4 mt-8">
+      <article className="bg-white rounded-lg shadow-md p-6">
+        <h1 className="text-3xl font-bold mb-4">{title}</h1>
+        <div className="mb-4 text-gray-600">
+          Published on <time dateTime="2023-10-01">October 1, 2023</time>
+        </div>
+        <Image
+          src=""
+          alt="Vercel Logo"
+          className="w-full h-auto mb-8 rounded"
+          priority
+        />
+        <div>
+          <BlogRichText body={body} />
+        </div>
+      </article>
+    </main>
+  );
 }
--- src/lib/RichText.jsx
+++ src/lib/RichText.jsx
@@ -1,11 +1,27 @@
-import { documentToReactComponents } from '@contentful/rich-text-react-renderer'
+import { BLOCKS, MARKS } from "@contentful/rich-text-types";
+import { documentToReactComponents } from "@contentful/rich-text-react-renderer";
+
+const Bold = ({ children }) => <p className="bold">{children}</p>;
+const Text = ({ children }) => <p className="align-center">{children}</p>;
+
+const options = {
+  renderMark: {
+    [MARKS.BOLD]: (text) => <Bold>{text}</Bold>,
+  },
+  renderNode: {
+    [BLOCKS.PARAGRAPH]: (node, children) => <Text>{children}</Text>,
+  },
+  renderText: (text) => text.replace("!", "?"),
+};
+
+documentToReactComponents(document, options);
 
 function RichText({ document }) {
-	if (!document) {
-		return null
-	}
+  if (!document) {
+    return null;
+  }
 
-	return <>{documentToReactComponents(document)}</>
+  return <>{documentToReactComponents(document)}</>;
 }
 
-export default RichText
+export default RichText;
